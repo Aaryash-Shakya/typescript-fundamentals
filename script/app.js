e5ea72a7356df27ae5bb6eef5ec3bd549d83c52c
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-08-10 19:32:09 +0545

    functions: rest, default

--- script/app.js
+++ script/app.js
@@ -10,24 +10,41 @@ let student = {
     name: "Aaryash",
     age: 20,
     address: "Lagan",
-    subject: subjects[subjects.WT]
+    subject: subjects[subjects.WT],
 };
 let studentsList = [
     {
         name: "Aditya",
         age: 25,
         address: "Kalanki",
-        subject: subjects[subjects.SAM]
+        subject: subjects[subjects.SAM],
     },
     {
         name: "Benup",
         age: 22,
         address: "Patan",
-        subject: subjects[subjects.SAD]
+        subject: subjects[subjects.SAD],
     },
 ];
 studentsList.push(student);
-studentsList.forEach((s) => {
-    console.log(`${s.name} live in ${s.address} and studies ${s.subject}`);
-});
+function getStudentsList(students) {
+    students.forEach((student) => {
+        console.log(`${student.name} live in ${student.address} and studies ${student.subject}`);
+    });
+}
+getStudentsList(studentsList);
+// Rest parameter - allow to pass a variable number of arguments of same type
+function getNumbers(...nums) {
+    nums.forEach((num) => {
+        console.log("number: " + num);
+    });
+}
+getNumbers(1, 2, 3, 4);
+getNumbers(33, 44);
+// Default parameter - assign default value to parameter if arg isn't passed
+function GetInfo(info = "happy") {
+    console.log(info);
+}
+GetInfo("sad");
+GetInfo();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 // arrays and enum
 
-enum subjects {SAD,SAM,WT}
+enum subjects {
+  SAD,
+  SAM,
+  WT,
+}
 
 let student = {
   name: "Aaryash",
   age: 20,
   address: "Lagan",
-  subject: subjects[subjects.WT]
+  subject: subjects[subjects.WT],
 };
 
 let studentsList = [
@@ -14,18 +18,39 @@ let studentsList = [
     name: "Aditya",
     age: 25,
     address: "Kalanki",
-    subject: subjects[subjects.SAM]
+    subject: subjects[subjects.SAM],
   },
   {
     name: "Benup",
     age: 22,
     address: "Patan",
-    subject: subjects[subjects.SAD]
+    subject: subjects[subjects.SAD],
   },
 ];
 
-studentsList.push(student)
+studentsList.push(student);
 
-studentsList.forEach((s) => {
-  console.log(`${s.name} live in ${s.address} and studies ${s.subject}`);
-});
+function getStudentsList(students: any[]) {
+  students.forEach((student) => {
+    console.log(
+      `${student.name} live in ${student.address} and studies ${student.subject}`
+    );
+  });
+}
+getStudentsList(studentsList);
+
+// Rest parameter - allow to pass a variable number of arguments of same type
+function getNumbers(...nums: number[]) {
+  nums.forEach((num) => {
+    console.log("number: " + num);
+  });
+}
+getNumbers(1, 2, 3, 4);
+getNumbers(33, 44);
+
+// Default parameter - assign default value to parameter if arg isn't passed
+function GetInfo(info: string = "happy") {
+  console.log(info);
+}
+GetInfo("sad");
+GetInfo();
